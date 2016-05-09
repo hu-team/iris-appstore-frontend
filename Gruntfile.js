b608a9349f2ddba9facea7a84ed1ab18c02a481a
Thierry Belliot <Thierrybelliot@gmail.com>
2016-05-09 21:06:46 +0200

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,31 @@ grunt.initConfig({
       dest: 'app/app.js'
     },
     external: {
-      src: ['external/*/*.js'],
+      src: ['external/jquery/*.js', 'external/angular/*.js', 'external/angular-sanitize/*.js', 'external/angular-ui-router/*.js', 'external/bootstrap-material-design/*.js'],
       dest: 'external/external.js'
     }
   },
   uglify: {
-    options: {
-      sourceMap: true,
-      beautify: true,
-      compress: {
-        drop_console: true
-      }
-    },
     app: {
+      options: {
+        sourceMap: true,
+        beautify: true,
+        compress: {
+          drop_console: true
+        }
+      },
       files: {
         'app/app.min.js': ['app/app.js']
       }
     },
     external: {
+      options: {
+        sourceMap: true,
+        beautify: false,
+        compress: {
+          drop_console: true
+        }
+      },
       files: {
         'external/external.min.js': ['external/external.js']
       }
@@ -49,19 +56,27 @@ grunt.initConfig({
 			}
 		}
 	},
+  less: {
+    app: {
+      files: {
+        'assets/css/style.css': 'assets/less/**.less'
+      }
+    }
+  },
   watch: {
     develop: {
-      files: ['app/*/**.js', 'app/*/*/*.js'],
-      tasks: ['concat:app', 'babel']
+      files: ['app/*/**.js', 'app/*/*/*.js', 'assets/less/**'],
+      tasks: ['concat:app', 'babel', 'less:app']
     }
   }
 });
 
-grunt.registerTask('build', ['concat:app', 'babel', 'concat:external', 'uglify:app', 'uglify:external', 'copy']);
+grunt.registerTask('build', ['concat:app', 'babel', 'concat:external', 'uglify:app', 'uglify:external', 'copy', 'less:app']);
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-babel');
 }
