321843dfb47ff1f7d7774007cee4102cf286be0f
Arjan <arjan.shojaeian@gmail.com>
2016-05-10 13:57:48 +0200

    First commit.

--- src/app/app.js
+++ src/app/app.js
@@ -4,13 +4,16 @@ angular.module('Arvici', ['ngSanitize', 'ui.router']);
 
 angular.module('Arvici').config(function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/Arvici");
-  $stateProvider.state('Arvici', {
-    url: "/Arvici",
+  $urlRouterProvider.otherwise("/");
+  $stateProvider.state('arvici', {
+    url: "/",
     templateUrl: "view/page/index.html"
-  }).state('Red', {
-    url: "/red",
-    templateurl: "view/red/red.html"
+  }).state('color', {
+    url: "/color",
+    templateUrl: "view/red/red.html",
+    controller: function controller($scope) {
+      $scope.items = ["Red", "Blue", "Yellow"];
+    }
   });
 });
 
