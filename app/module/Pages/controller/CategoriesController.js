c51a76231b93082785c4670015a2b6cee4d6b6ae
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-05-17 11:44:26 +0200

    Hide category at login

--- app/module/Pages/controller/CategoriesController.js
+++ app/module/Pages/controller/CategoriesController.js
@@ -1,5 +1,17 @@
-angular.module('Arvici').controller('CategoriesController', ['appService', '$scope', function(appService, $scope){
-  $scope.items = {};
+angular.module('Arvici').controller('CategoriesController', ['appService', '$scope', '$state', function(appService, $scope, $state) {
+
+$scope.loginstate = false;
+  $scope.$on('$stateChangeSuccess',
+    function(event, unfoundState, fromState, fromParams){
+      if($state.current.name === "login") {
+        $scope.loginstate = true;
+      } else {
+        $scope.loginstate = false;
+      }
+        //console.log($state.current.name); // "lazy.state"
+        //console.log(unfoundState.toParams); // {a:1, b:2}
+        //console.log(unfoundState.options); // {inherit:false} + default options
+  });
 
   function init() {
     appService.getCategory().then(function(data){
