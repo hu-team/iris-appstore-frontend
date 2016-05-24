4999709a6ebaac5df1828a7b48d5eca776890f4e
Thierry Belliot <thierry.belliot@student.hu.nl>
2016-05-24 14:02:45 +0200

    materail design shizzle

--- app/config/config.js
+++ app/config/config.js
@@ -6,7 +6,7 @@ function setEnv() {
   var env = "develop";
 
   if(env === "develop") {
-    angular.module('Arvici').value('API_PATH', 'http://10.200.200.17/APPSTORE_ARJAN_WS/Api');
+    angular.module('Arvici').value('API_PATH', 'http://10.200.200.17/APPSTORE_OLIVIER_WS/Api');
   } else {
     angular.module('Arvici').value('API_PATH', 'http://10.200.200.17/APPSTORE/Api')
   }
